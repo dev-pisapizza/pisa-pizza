86ffa523173386321e3b5defa4f751843b329d4d
Oscar Rosner <oscar.rosner@web.de>
2024-01-22 17:37:25 +0100

    f

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -43,9 +43,9 @@ export class AppComponent implements OnInit {
   private isInView(element: HTMLElement) {
     const rect = element.getBoundingClientRect();
     return (
-      rect.top >= 0 &&
+      rect.bottom >= 0 &&
+      rect.top <= (window.innerHeight || document.documentElement.clientHeight) &&
       rect.left >= 0 &&
-      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
       rect.right <= (window.innerWidth || document.documentElement.clientWidth)
     );
   }
