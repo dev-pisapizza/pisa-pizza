7f4b35831046a6aae1ebe343cd00ad31f36df9ec
Oscar Rosner <oscar.rosner@web.de>
2024-01-22 17:43:11 +0100

    better fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -40,13 +40,8 @@ export class AppComponent implements OnInit {
     }
   }
 
-  private isInView(element: HTMLElement) {
+  isInView(element: HTMLElement) {
     const rect = element.getBoundingClientRect();
-    return (
-      rect.bottom >= 0 &&
-      rect.top <= (window.innerHeight || document.documentElement.clientHeight) &&
-      rect.left >= 0 &&
-      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-    );
+    return !(rect.bottom < 0);
   }
 }
