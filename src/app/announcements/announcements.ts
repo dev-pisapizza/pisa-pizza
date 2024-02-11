d157f8eaa27b7644e98a1631f4e048cf81981820
Oscar Rosner <oscar.rosner@web.de>
2024-02-11 19:59:21 +0100

    fix

--- src/app/announcements/announcements.ts
+++ src/app/announcements/announcements.ts
@@ -10,7 +10,7 @@ export const announcements: Announcement[] = [
     description:
       'Unsere Pizzeria ist vom <strong>11.03 - 05.03.2024</strong> geschlossen. Wir sind ab dem <strong>6. März</strong> wieder für Sie da!',
     showFromDate: '24.12.2023', // ab wann es angezeigt werden soll
-    showUntilDate: '11.03.2024', // ab wann es nicht mehr angezeigt werden soll (10.01.2024 -> wird ab 10.01 um 0 Uhr nicht mehr gezeigt)
+    showUntilDate: '11.01.2024', // ab wann es nicht mehr angezeigt werden soll (10.01.2024 -> wird ab 10.01 um 0 Uhr nicht mehr gezeigt)
     closeable: false,
   },
 ];
